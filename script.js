dc0c8d285b5def0089499cdd3d3611bd1bc9cf7d
SnardDog <SnardDog@proton.me>
2023-01-27 05:47:52 -0600

    Added background toggle

---
+++ script.js
@@ -0,0 +1,16 @@
+const bgSelect = document.getElementById("bgSelect");
+const bgStyle = document.body.classList
+
+function backgroundChange() {
+if (bgSelect.value == "ironForge") {
+    document.body.classList = "ironForge";
+}
+else if (bgSelect.value = "onyxia") {
+    document.body.classList = "onyxia";
+}
+
+}
+
+
+
+
